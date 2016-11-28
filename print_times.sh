22201025d0a6cfe11c46d56d27e9ed177f443963
gaburiero <gaburiero.c@gmail.com>
2016-11-28 10:26:22 -0200

    modify print_times script to delet result.txt, recompile .c program and take a mean value of the times

--- print_times.sh
+++ print_times.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+rm -rf result.txt
+
+gcc dijkstra-original-randGraph.c -lm -lpthread
+
 for (( i=10; i<=40000; i=i+1000 ))
 do
-    ./a.out $i >> result.txt
+  cnt=0
+  for ((j=0; j<=10; j++))
+  do
+    run=$(./a.out $i)
+    cnt=$(echo "${cnt}+${run}"|bc)
+  done
+  mean_avg=$(echo "${cnt}/10" | bc -l)
+  mean_avg=$(echo ${mean_avg} | cut -b1-6)
+  echo "$i $mean_avg"
+  echo "$i $mean_avg" >> result.txt
 done
