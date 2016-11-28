46ba07d85995c2d9caf3af2a6cc1b3af976d4235
gaburiero <gaburiero.c@gmail.com>
2016-11-28 10:25:17 -0200

    modify python plot application to compare differentdata files

--- plot.py
+++ plot.py
@@ -11,7 +11,11 @@ ax1.set_title("Dijkstra Serial Execution")
 ax1.set_xlabel('Number of Vertices')
 ax1.set_ylabel('Time (ms)')
 
-plt.plot(*np.loadtxt("data_serial.dat",unpack=True), linewidth=2.0)#, label='serial')
+plt.plot(*np.loadtxt("data_serial.dat",unpack=True), 'b', linewidth=2.0,label='serial')
+plt.plot(*np.loadtxt("result.txt",unpack=True), 'g', linewidth=2.0,label='serial_mean')
+#plt.plot(*np.loadtxt("data_pthread2.dat",unpack=True), 'g', linewidth=2.0, label='pthread2')
+#plt.plot(*np.loadtxt("data_pthread4.dat",unpack=True), 'r', linewidth=2.0, label='pthread4')
+#plt.plot(*np.loadtxt("data_pthread8.dat",unpack=True), 'k', linewidth=2.0, label='pthread4')
 
 leg = ax1.legend()
 
