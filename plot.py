0a7fdca8c06a9cda6a4f9e9ca3bdffa776edb949
gaburiero <gaburiero.c@gmail.com>
2016-12-07 10:53:00 -0200

    change file location for plot

--- plot.py
+++ plot.py
@@ -11,25 +11,25 @@ ax1.set_title("Dijkstra Serial Execution")
 ax1.set_xlabel('Number of Vertices')
 ax1.set_ylabel('Time (ms)')
 
-plt.plot(*np.loadtxt("data_serialtp.dat",unpack=True), linewidth=2.0,label='serial')
+plt.plot(*np.loadtxt("data_files/data_serialtp.dat",unpack=True), linewidth=2.0,label='serial')
 
 #pthread
 
 if 1:
-    plt.plot(*np.loadtxt("data_pthread2tp.dat",unpack=True), linewidth=2.0, label='pthread2')
-    plt.plot(*np.loadtxt("data_pthread4tp.dat",unpack=True), linewidth=2.0, label='pthread4')
-    plt.plot(*np.loadtxt("data_pthread8tp.dat",unpack=True), linewidth=2.0, label='pthread8')
+    plt.plot(*np.loadtxt("data_files/data_pthread2tp.dat",unpack=True), linewidth=2.0, label='pthread2')
+    plt.plot(*np.loadtxt("data_files/data_pthread4tp.dat",unpack=True), linewidth=2.0, label='pthread4')
+    plt.plot(*np.loadtxt("data_files/data_pthread8tp.dat",unpack=True), linewidth=2.0, label='pthread8')
 
 #mpi
 if 0:
-    plt.plot(*np.loadtxt("data_mpi2tp.dat",unpack=True), linewidth=2.0, label='mpi 2')
-    plt.plot(*np.loadtxt("data_mpi4tp.dat",unpack=True), linewidth=2.0, label='mpi 4')
-    plt.plot(*np.loadtxt("data_mpi8tp.dat",unpack=True), linewidth=2.0, label='mpi 8')
+    plt.plot(*np.loadtxt("data_files/data_mpi2tp.dat",unpack=True), linewidth=2.0, label='mpi 2')
+    plt.plot(*np.loadtxt("data_files/data_mpi4tp.dat",unpack=True), linewidth=2.0, label='mpi 4')
+    plt.plot(*np.loadtxt("data_files/data_mpi8tp.dat",unpack=True), linewidth=2.0, label='mpi 8')
 
 #mpi+pthread
 if 0:
-    plt.plot(*np.loadtxt("data_mpit2_4tp.dat",unpack=True), linewidth=2.0, label='mpi 2 + pthread 4')
-    plt.plot(*np.loadtxt("data_mpit2_2tp.dat",unpack=True), linewidth=2.0, label='mpi 2 + pthread 2')
+    plt.plot(*np.loadtxt("data_files/data_mpit2_4tp.dat",unpack=True), linewidth=2.0, label='mpi 2 + pthread 4')
+    plt.plot(*np.loadtxt("data_files/data_mpit2_2tp.dat",unpack=True), linewidth=2.0, label='mpi 2 + pthread 2')
     #plt.plot(*np.loadtxt("data_mpt8tp.dat",unpack=True), linewidth=2.0, label='mpi 8 + pthread')
 
 leg = ax1.legend(loc=2)
