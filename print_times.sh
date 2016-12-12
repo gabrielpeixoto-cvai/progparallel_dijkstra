d1e1cd855b849ae43a3bcac7ddf413e34d25f488
gaburiero <gaburiero.c@gmail.com>
2016-12-12 21:19:32 -0200

    added more options

--- print_times.sh
+++ print_times.sh
@@ -2,30 +2,40 @@
 rm -rf result.txt
 
 #serial
-#gcc dijkstra-original-randGraph.c -lm -lpthread
+#gcc -O2 dijkstra-original-randGraph.c -lm -lpthread
+
+#pthread -O2
+#gcc -O2 dijkstra-pthread.c -lm -lpthread
 
 #pthread
 #gcc dijkstra-pthread.c -lm -lpthread
 
+#if openmpi -O2 without pthread
+#mpicc -O2 dijkstra-bcast-nopthread.c -lm
+
 #if openmpi without pthread
-#mpicc dijkstra-bcast-nopthread.c -lm
+mpicc dijkstra-bcast-nopthread.c -lm
+
+#if openmpi+pthread -O2
+#mpicc -O2 dijkstra-bcast.c -lm -lpthread
 
 #if openmpi+pthread
-mpicc dijkstra-bcast.c -lm -lpthread
+#mpicc dijkstra-bcast.c -lm -lpthread
 
 for (( i=24; i<=32024; i=i+1000 ))
 do
   cnt=0
-  for ((j=0; j<=10; j++))
+  for ((j=0; j<=5; j++))
   do
     #if pthread
-    #run=$(./a.out $i)
+    #run=$(./a.out $i )
     #if openmpi
     run=$( mpiexec -n 2 a.out $i )
     cnt=$(echo "${cnt}+${run}"|bc)
   done
-  mean_avg=$(echo "${cnt}/10" | bc -l)
-  mean_avg=$(echo ${mean_avg} | cut -b1-6)
+  mean_avg=$(echo "${cnt}/5" | bc -l)
+  mean_avg=$(echo ${mean_avg} )
+  #mean_avg=$(echo ${mean_avg} | cut -b1-6)
   echo "$i $mean_avg"
   echo "$i $mean_avg" >> result.txt
 done
